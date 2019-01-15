bccd2530222d0e0d294fadc2cc7a9cbd9cae34dc
Adam Butterworth <abutterworth@edx.org>
2019-01-15 10:26:26 -0500

    Updates

---
+++ precache-manifest.cd40baab4b8c8034497200a954c7c965.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "92278083e057b83c5357",
+    "url": "/playing-with-carbon/static/css/main.9d401eed.chunk.css"
+  },
+  {
+    "revision": "92278083e057b83c5357",
+    "url": "/playing-with-carbon/static/js/main.92278083.chunk.js"
+  },
+  {
+    "revision": "570a2a5f63763b4ecccd",
+    "url": "/playing-with-carbon/static/js/1.570a2a5f.chunk.js"
+  },
+  {
+    "revision": "5395ad41f698cd8f12db",
+    "url": "/playing-with-carbon/static/js/runtime~main.5395ad41.js"
+  },
+  {
+    "revision": "4f5ef9f72f9d8e502b593a3d9b403d85",
+    "url": "/playing-with-carbon/index.html"
+  }
+];
